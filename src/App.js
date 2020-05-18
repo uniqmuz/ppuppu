918cb7a33ebaae4fe7e2d493c16683d30c75f64b
uniqmuz <sudogo00@gmail.com>
2020-05-18 21:34:08 +0900

    initial commit
    Signed-off-by: uniqmuz <sudogo00@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+import './custom.scss';
 
+//--slider-height-percentage: 43%;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <img style={{ width: '100%' }} src="/static/imgs/intro.jpg" alt="" />
+      <img style={{ width: '100%' }} src="/static/imgs/info.jpg" alt="" />
+      <AwesomeSlider
+        className="aws-btn"
+        bullets={false}
+      >
+        <div data-src="./static/imgs/1.jpg" />
+        <div data-src="./static/imgs/2.jpg" />
+        <div data-src="./static/imgs/3.jpg" />
+        <div data-src="./static/imgs/4.jpg" />
+        <div data-src="./static/imgs/5.jpg" />
+        <div data-src="./static/imgs/6.jpg" />
+        <div data-src="./static/imgs/7.jpg" />
+        <div data-src="./static/imgs/8.jpg" />
+        <div data-src="./static/imgs/9.jpg" />
+      </AwesomeSlider>
+    </>
   );
 }
 
