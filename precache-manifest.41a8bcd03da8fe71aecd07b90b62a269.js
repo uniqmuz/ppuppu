beddf0e03d2af637541fde4745516d83fa1c9804
uniqmuz <sudogo00@gmail.com>
2020-05-18 21:56:39 +0900

    Updates

---
+++ precache-manifest.41a8bcd03da8fe71aecd07b90b62a269.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b27196c8678019fd4b266d547c1d2c8",
+    "url": "/ppuppu/index.html"
+  },
+  {
+    "revision": "03a1257d918968c3898c",
+    "url": "/ppuppu/static/css/2.8f118048.chunk.css"
+  },
+  {
+    "revision": "4a8d275eccd285d05c9d",
+    "url": "/ppuppu/static/css/main.416ad669.chunk.css"
+  },
+  {
+    "revision": "03a1257d918968c3898c",
+    "url": "/ppuppu/static/js/2.80aab02c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ppuppu/static/js/2.80aab02c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a8d275eccd285d05c9d",
+    "url": "/ppuppu/static/js/main.e0cef25c.chunk.js"
+  },
+  {
+    "revision": "e85545e497f92c02defc",
+    "url": "/ppuppu/static/js/runtime-main.70cc37ce.js"
+  }
+]);
